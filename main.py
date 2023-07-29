5e2b2bb45d26b1f04898c6a51c3d0ad06f069c85
Jamie <dzami3@gmail.com>
2023-07-28 22:49:33 -0700

    adding code

---
+++ main.py
@@ -0,0 +1,16 @@
+import re
+
+
+
+with open("find.txt","r") as f, open("repl.txt","r") as r, open("input.txt","r") as i, open("output.txt","w") as o: 
+    find = f.readlines()
+    repl = r.readlines()
+    inpt = i.read()
+    if len(find) != len(repl):
+        raise RuntimeError("Find and Replace files different lengths")
+    for x in range(len(find)):
+        # strip off the \n
+        find[x] = find[x][:-1]
+        repl[x] = repl[x][:-1]
+        inpt = re.sub(find[x],repl[x],inpt)
+    o.write(inpt)
